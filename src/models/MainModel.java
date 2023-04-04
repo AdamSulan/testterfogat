ff214f263cc16ab7e63fbc928ed89462875efccd
SulanAdam <sulan.adam19@gmail.com>
2023-04-04 14:29:12 +0200

    MainModel, MainPanel elkészült

--- src/models/MainModel.java
+++ src/models/MainModel.java
@@ -11,5 +11,8 @@
 package models;
 
 public class MainModel {
-    
+    public double calcVolume(double testAtlo){
+        double volume=Math.pow(testAtlo, 3)/Math.pow(27, 1/2);
+        return volume;
+    }
 }
--- src/views/MainPanel.java
+++ src/views/MainPanel.java
@@ -10,6 +10,45 @@
 
 package views;
 
-public class MainPanel {
+import com.github.suli2022.inputlib.ButtonPanel;
+import com.github.suli2022.inputlib.InputPanel;
+import com.github.suli2022.inputlib.TitlePanel;
+
+import javafx.scene.layout.VBox;
+
+public class MainPanel extends VBox {
+    TitlePanel titlePanel;
+    InputPanel spaceDiagonalPanel;
+    ButtonPanel buttonPanel;
+    InputPanel volumePanel;
+
+    public MainPanel(){
+        this.titlePanel=new TitlePanel();
+        this.spaceDiagonalPanel=new InputPanel();
+        this.buttonPanel=new ButtonPanel();
+        this.volumePanel=new InputPanel();
+
+        this.titlePanel.setText("Kocka térfogata");
+        this.spaceDiagonalPanel.setText("A kocka testátlója");
+        this.buttonPanel.setCalcButtonText("Számít");
+        this.volumePanel.setText("Terület");
+
+        this.getChildren().add(this.titlePanel);
+        this.getChildren().add(this.spaceDiagonalPanel);
+        this.getChildren().add(this.buttonPanel);
+        this.getChildren().add(this.volumePanel);
+    }
     
+    public TitlePanel getTitlePanel() {
+        return titlePanel;
+    }
+    public InputPanel getSpaceDiagonalPanel() {
+        return spaceDiagonalPanel;
+    }
+    public ButtonPanel getButtonPanel() {
+        return buttonPanel;
+    }
+    public InputPanel getVolumePanel() {
+        return volumePanel;
+    }
 }
