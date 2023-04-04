f342d01f1b03ff17e45265cb1ae16c7cb444a450
SulanAdam <sulan.adam19@gmail.com>
2023-04-04 14:13:24 +0200

    file létrehozva

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/*
+* File: App.java
+* Author: Sulan Ádám
+* Copyright: 2023, Sulan Ádám
+* Group: Szoft I-1-N
+* Date: 2023-04-04
+* Github: https://github.com/AdamSulan/testterfogat.git
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
